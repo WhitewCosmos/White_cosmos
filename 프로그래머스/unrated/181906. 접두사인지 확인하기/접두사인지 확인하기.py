f03a51225be9f72a5e7719ac5c5e6a151107ee6d
White_cosmos <76742694+WhitewCosmos@users.noreply.github.com>
2023-11-15 20:17:41 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,9 +1,9 @@
 def solution(my_string, is_prefix):
     answer = 0
-    for i, k in enumerate(is_prefix):
-        if i < len(my_string):
+    if len(is_prefix) > len(my_string):
+        return 0
+    else:
+        for i, k in enumerate(is_prefix):
             if k != my_string[i]:
                 return answer
-        else:
-            return answer
-    return 1       
+        return 1       
